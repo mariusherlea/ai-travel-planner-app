ec9f18160078081cd531f05d2c247d16592cfef4
Herlea Marius <mariusherlea@gmail.com>
2024-11-11 09:57:06 +0200

    signIn signUp page

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,19 +1,21 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
-import { useNavigation } from "expo-router";
+import { useNavigation, useRouter } from "expo-router";
 import { Colors } from "@/constants/Colors";
 import { TextInput } from "react-native";
 import { StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function SignIn() {
-  // const navigation = useNavigation();
+  const navigation = useNavigation();
 
-  // useEffect(() => {
-  //   navigation.setOptions({
-  //     title: "Sign In",
-  //   });
-  // }, []);
-
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  const router = useRouter();
   return (
     <View
       style={{
@@ -23,16 +25,58 @@ export default function SignIn() {
         backgroundColor: Colors.WHITE,
       }}
     >
-      <Text style={{ fontSize: 30 }}>Let's Sign You In</Text>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+
+      <Text style={{ fontSize: 30, marginTop: 30 }}>Let's Sign You In</Text>
       <Text style={{ fontSize: 25, color: Colors.GREY, marginTop: 20 }}>
         Welcome back
       </Text>
       <Text style={{ fontSize: 30, color: Colors.GREY, marginTop: 10 }}>
         You've been missed
       </Text>
+      {/* Email */}
       <View style={{ marginTop: 50 }}>
         <Text>Email</Text>
         <TextInput style={styles.input} placeholder="Enter Email" />
+      </View>
+      {/* Password */}
+      <View style={{ marginTop: 50 }}>
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Enter Password"
+        />
+      </View>
+      <View>
+        {/* Sign In Button */}
+        <View
+          style={{
+            padding: 15,
+            marginTop: 50,
+            backgroundColor: Colors.PRIMARY,
+            borderRadius: 15,
+          }}
+        >
+          <Text style={{ color: Colors.WHITE, textAlign: "center" }}>
+            Sign In
+          </Text>
+        </View>
+        {/* Create account Button */}
+        <TouchableOpacity
+          onPress={() => router.replace("auth/sign-up")}
+          style={{
+            padding: 15,
+            marginTop: 20,
+            backgroundColor: Colors.WHITE,
+            borderRadius: 15,
+            borderWidth: 1,
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>Create Account</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- app/auth/sign-up/index.js
+++ app/auth/sign-up/index.js
@@ -1,10 +1,90 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigation, useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { TextInput } from "react-native";
+import { StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function SignUp() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  const router = useRouter();
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View
+      style={{
+        padding: 25,
+        height: "100%",
+        paddingTop: 50,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+      <Text style={{ fontSize: 30, paddingTop: 40 }}>Create New Account</Text>
+      {/* User Full Name */}
+      <View style={{ marginTop: 50 }}>
+        <Text>Full Name</Text>
+        <TextInput style={styles.input} placeholder="Enter Full Name" />
+      </View>
+      {/* Email */}
+      <View style={{ marginTop: 20 }}>
+        <Text>Email</Text>
+        <TextInput style={styles.input} placeholder="Enter Email" />
+      </View>
+      {/* Password */}
+      <View style={{ marginTop: 20 }}>
+        <Text>Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Enter Password"
+        />
+      </View>
+      <View>
+        {/* Create account Button */}
+        <View
+          style={{
+            padding: 15,
+            marginTop: 50,
+            backgroundColor: Colors.PRIMARY,
+            borderRadius: 15,
+          }}
+        >
+          <Text style={{ color: Colors.WHITE, textAlign: "center" }}>
+            Create Account
+          </Text>
+        </View>
+        {/* Create account Button */}
+        <TouchableOpacity
+          onPress={() => router.replace("auth/sign-in")}
+          style={{
+            padding: 15,
+            marginTop: 20,
+            backgroundColor: Colors.WHITE,
+            borderRadius: 15,
+            borderWidth: 1,
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderColor: Colors.GREY,
+    borderRadius: 15,
+  },
+});
