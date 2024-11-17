6f66910acec4755310d6aad0330716fd20fb2846
Herlea Marius <mariusherlea@gmail.com>
2024-11-17 10:47:03 +0200

    mytrip(homepage)

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,44 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Feather } from "@expo/vector-icons";
+import { Colors } from "@/constants/Colors";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="mytrip" />
-      <Tabs.Screen name="discover" />
-      <Tabs.Screen name="profile" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors.PRIMARY,
+      }}
+    >
+      <Tabs.Screen
+        name="mytrip"
+        options={{
+          tabBarLabel: "My Trip",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="location-sharp" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="discover"
+        options={{
+          tabBarLabel: "Discover",
+          tabBarIcon: ({ color }) => (
+            <Feather name="globe" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="people" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,38 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useState } from "react";
 
 export default function MyTrip() {
+  const [userTrips, setUserTrips] = useState([]);
+
   return (
-    <View>
-      <Text>MyTrip</Text>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 55,
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={{ fontSize: 35 }}>My Trip</Text>
+        <Ionicons name="add-circle" size={50} color="black" />
+      </View>
+      {userTrips.length > 0 ? (
+        <View>
+          <Text>You have no trips</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>You have no trips</Text>
+        </View>
+      )}
     </View>
   );
 }
