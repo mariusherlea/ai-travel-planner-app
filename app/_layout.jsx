70c2a373c627289a6c54ccdf77dce3453b6c4d50
Herlea Marius <mariusherlea@gmail.com>
2024-11-19 09:48:08 +0200

    CreateTripContext

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { CreateTripContext } from "../context/CreateTripContext";
+import { useState } from "react";
+
+export default function RootLayout() {
+  const [tripData, setTripData] = useState({});
+  return (
+    <CreateTripContext.Provider value={{ tripData, setTripData }}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </CreateTripContext.Provider>
+  );
+}
--- app/create-trip/search-place.jsx
+++ app/create-trip/search-place.jsx
@@ -1,14 +1,16 @@
 import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { useNavigation } from "expo-router";
 import { Colors } from "@/constants/Colors";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { v4 as uuidv4 } from "uuid";
 import "react-native-get-random-values";
+import { CreateTripContext } from "@/context/CreateTripContext";
 
 export default function searchPlace() {
   uuidv4();
   const navigation = useNavigation();
+  const { tripData, setTripData } = useContext(CreateTripContext);
   useEffect(() => {
     navigation.setOptions({
       headerShown: true,
@@ -16,6 +18,9 @@ export default function searchPlace() {
       headerTitle: "Search",
     });
   }, []);
+  useEffect(() => {
+    console.log(tripData);
+  }, [tripData]);
   return (
     <View
       style={{
@@ -26,15 +31,52 @@ export default function searchPlace() {
       }}
     >
       <GooglePlacesAutocomplete
-        placeholder="Search"
+        placeholder="Search Place"
         fetchDetails={true}
+        onFail={(error) => console.error(error)}
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
-          console.log(data, details);
+          console.log(data.description);
+          console.log(details?.geometry?.location);
+          console.log(details?.photos[0]?.photo_reference);
+          console.log(details?.url);
+          setTripData({
+            // ...tripData,
+            // placeId: data.place_id,
+            // placeName: data.description,
+            // placeLocation: details?.geometry?.location,
+            // placeImage: details?.photos[0]?.photo_reference,
+            // placeUrl: details?.url,
+            locationInfo: {
+              name: data.description,
+              coordinates: details?.geometry?.location,
+              photo_reference: details?.photos[0]?.photo_reference,
+              url: details?.url,
+            },
+          });
         }}
         query={{
           key: process.env.GOOGLE_MAP_API_KEY,
           language: "en",
+        }}
+        styles={{
+          container: {
+            flex: 0,
+            marginTop: 20,
+          },
+          textInput: {
+            backgroundColor: Colors.LIGHT_GRAY,
+            borderRadius: 10,
+            paddingHorizontal: 15,
+            fontSize: 16,
+          },
+          textInputContainer: {
+            backgroundColor: Colors.LIGHT_GRAY,
+            borderRadius: 10,
+            paddingHorizontal: 15,
+            fontSize: 16,
+            borderWidth: 1,
+          },
         }}
       />
     </View>
