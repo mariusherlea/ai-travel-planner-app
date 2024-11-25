b37caa2f540dfe139869bd0f884dda8cb9cc98b6
Herlea Marius <mariusherlea@gmail.com>
2024-11-25 12:09:18 +0200

    select date and calculate the time difference

--- app/create-trip/select-dates.jsx
+++ app/create-trip/select-dates.jsx
@@ -1,55 +1,68 @@
 import React, { Component } from "react";
-import { TouchableOpacity } from "react-native";
+import { ToastAndroid, TouchableOpacity } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import CalendarPicker from "react-native-calendar-picker";
 import { Colors } from "@/constants/Colors";
 import { Link } from "expo-router";
+import { useState } from "react";
+import moment from "moment";
 
-export default class App extends Component {
-  onDateChange(date, type) {
+export default function SelectDate() {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  onDateChange = (date, type) => {
     console.log(date, type);
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={{ fontSize: 35, padding: 20 }}>Travel dates</Text>
-        <CalendarPicker
-          onDateChange={this.onDateChange}
-          allowRangeSelection={true}
-          minDate={new Date()}
-        />
-        <TouchableOpacity
-          style={{
-            padding: 15,
-            backgroundColor: Colors.PRIMARY,
-            borderRadius: 15,
-            marginTop: 20,
-          }}
-          onPress={onDateSelectContinue}
-        >
-          {/* <Link
+    if (type == "START_DATE") {
+      setStartDate(moment(date));
+    } else {
+      setEndDate(moment(date));
+    }
+  };
+
+  const onDateSelectContinue = () => {
+    if (!startDate && !endDate) {
+      ToastAndroid.show("Please select dates", ToastAndroid.LONG);
+      return;
+    }
+    const totalNoOfDays = endDate.diff(startDate, "days");
+    console.log(totalNoOfDays + 1);
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 35, padding: 20 }}>Travel dates</Text>
+      <CalendarPicker
+        onDateChange={this.onDateChange}
+        allowRangeSelection={true}
+        minDate={new Date()}
+      />
+      <TouchableOpacity
+        style={{
+          padding: 15,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 15,
+          marginTop: 20,
+        }}
+        onPress={onDateSelectContinue}
+      >
+        {/* <Link
             href={"/create-trip/select-dates"}
             style={{ textAlign: "center" }}
           > */}
-          <Text
-            style={{
-              justifyContent: "center",
-              color: Colors.WHITE,
-              alignContent: "center",
-            }}
-          >
-            Continue
-          </Text>
-          {/* </Link> */}
-        </TouchableOpacity>
-      </View>
-    );
-  }
+        <Text
+          style={{
+            justifyContent: "center",
+            color: Colors.WHITE,
+            alignContent: "center",
+          }}
+        >
+          Continue
+        </Text>
+        {/* </Link> */}
+      </TouchableOpacity>
+    </View>
+  );
 }
-
-const onDateSelectContinue = () => {
-  console.log("continue");
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -58,10 +71,8 @@ const styles = StyleSheet.create({
     padding: 5,
     display: "flex",
     flexDirection: "column",
-    justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: Colors.YELLOW,
-    marginTop: 5,
     borderRadius: 15,
   },
 });
